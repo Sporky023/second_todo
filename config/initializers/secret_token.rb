a1d31daf723a94cd177aba3a9dd0de0b9de56d89
Luke Griffiths <wlgriffiths@gmail.com>
2012-03-06 10:25:05 -0500

    fist I ran rails new second_todo and then added backbone-on-rails gem too

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondTodo::Application.config.secret_token = 'b825281fef8e0031199c680f4bae8b26f85c79f251f671e1a8bd5b1349dae553001116a7c24573250d834fca5f7fbd6de6d3579618e68ba722dbcaefbfacbd54'
